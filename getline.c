f63b39ee845582a8d3776dea0405255f5ebc6314
Elias Fleckenstein <eliasfleckenstein@web.de>
2022-04-24 18:09:32 +0200

    Export getline signature

--- getline.c
+++ getline.c
@@ -7,9 +7,6 @@
 #include <errno.h>
 #include "getline.h"
 
-// if typedef doesn't exist (msvc, blah)
-typedef intptr_t ssize_t;
-
 ssize_t getline(char **lineptr, size_t *n, FILE *stream) {
     size_t pos;
     int c;
--- getline.h
+++ getline.h
@@ -4,4 +4,9 @@
 #include <stdio.h>
 #include <stdint.h>
 
+// if typedef doesn't exist (msvc, blah)
+typedef intptr_t ssize_t;
+
+ssize_t getline(char **lineptr, size_t *n, FILE *stream);
+
 #endif // _GETLINE_H_
